537f6bb32364693dc93abde7075de7d69a17f75b
bendikbp <48202511+bendikbp@users.noreply.github.com>
2026-03-09 10:10:52 +0100

    Refactor globals.h to include motion calibration

--- FCV4/globals.h
+++ FCV4/globals.h
@@ -1,35 +1,29 @@
 #pragma once
 #include <Arduino.h>
-
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL375.h>
 #include <Adafruit_ICM20649.h>
 #include <Adafruit_LIS3MDL.h>
 #include <Adafruit_BMP3XX.h>
-
 #include "calibration_data.h"
+#include "motioncal_eeprom.h"
 
-
-
-// ------------------- Hardware pins -------------------
+// Hardware pins
 extern const int           SPI_SCK;
 extern const int           SPI_MISO;
 extern const int           SPI_MOSI;
 extern const int           CS_ADXL;
 extern const int           CS_ICM;
 
-
-
-// ------------------- Sensors -------------------
+// Sensors
 extern Adafruit_ADXL375    adxl;
 extern Adafruit_ICM20649   icm;
 extern Adafruit_LIS3MDL    lis3mdl;
 extern Adafruit_BMP3XX     bmp;
 
-
-
-// ------------------- Calibration -------------------
+// Calibration
 extern CalibrationData     CAL;
+extern MotionCalData       MCAL;
 
 extern float               gyro_offset_x, gyro_offset_y, gyro_offset_z;
 extern float               adxl_xOff, adxl_yOff, adxl_zOff;
@@ -40,15 +34,13 @@ extern float               bmp_p0_Pa;
 extern bool                bmp_present;
 
 
-// ------------------- Attitude -------------------
-extern float               roll, pitch, yaw;     // deg
+// Attitude
+extern float               roll, pitch, yaw;  // deg
 extern float               yaw_offset;
 extern float               alpha_rp;
 extern unsigned long       lastMicros;
 
-
-
-// ------------------- Misc -------------------
+// Annet
 extern unsigned long       seq;
 extern float               g0;
 extern unsigned long       LIFT_OFFStart;
@@ -57,10 +49,7 @@ extern int                 lift_off_armed;
 extern bool                drogueFired;
 extern bool                mainFired;
 
-
-
-
-// ------------------- Launch detect tuning -------------------
+// Launch detect tuning
 extern const float         LAUNCH_A_NORM;
 extern const int           LAUNCH_N_NORM;
 
@@ -70,9 +59,7 @@ extern const int           LAUNCH_N_HARD;
 extern const unsigned long ARM_QUIET_MS;
 extern const unsigned long ARM_TIMEOUT_MS;
 
-
-
-// ------------------- Kalman VZ -------------------
+// Kalman VZ
 extern float               z_est, v_est;
 extern float               P00, P01, P10, P11;
 
@@ -81,9 +68,7 @@ extern float               sigma_model_v;
 extern float               sigma_baro;
 extern float               R_baro;
 
-
-
-// ------------------- FSM -------------------
+// FSM
 enum State {
   SYSTEM_CHECK = 1,
   OPERATION_READY,
@@ -94,6 +79,7 @@ enum State {
   MAIN_DEPLOY,
   MAIN_DESCENT
 };
+
 extern                     State state;
 extern                     State lastState;
 extern unsigned long       stateStart;
